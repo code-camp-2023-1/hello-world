5c7088ed2fdec7cde1badee707bc6ec8ab5970a1
Xitadi-Manfu <1791037454@qq.com>
2023-07-27 07:50:16 +0800

    作业

--- src/main/java/com/jirengu/HelloWorld.java
+++ src/main/java/com/jirengu/HelloWorld.java
@@ -2,5 +2,8 @@ package com.jirengu;
 
 public class HelloWorld {
     // 在这里创建一个主方法, 打印Hello World
-
+    // 主方法入口
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
